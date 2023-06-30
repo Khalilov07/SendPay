b87e3f21df7d58e3d04cbcd9a43d2c3ef34f2fe6
Marka <khalilovakram07@gmail.com>
2023-06-30 18:55:52 +0600

    add project

---
+++ script/script.js
@@ -0,0 +1,54 @@
+// $('.works-carousel').owlCarousel({
+//     loop:true ,
+//     margin:10,
+//     nav:false,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:3
+//         }
+//     }
+// })
+
+// true / false
+
+$('.works-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('.about__us__comments').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:2.5
+        }
+    }
+})
+
+// true / false
